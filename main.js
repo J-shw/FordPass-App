21f5f4fb704a96b753fb969c8c9e3480a32b1ecc
Josh <91017208+J-shw@users.noreply.github.com>
2024-03-27 21:54:26 +0000

    Getting slightly closer

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const {app,BrowserWindow, ipcMain }=require('electron')
+const {app,BrowserWindow,protocol, ipcMain }=require('electron')
 const path=require('path')
 const auth = require('./modules/auth');
 
